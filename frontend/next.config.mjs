2034704d26b6db769b4068a69664daf33d547812
SUMAN JANA <sumanjanaled@gmail.com>
2025-04-25 22:04:09 +0530

    final try

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack(config) {
+    config.resolve = {
+      ...config.resolve,
+      alias: {
+        ...config.resolve.alias,
+        '@': '.',
+        '@/lib': './lib',
+        '@/components': './components',
+        '@/app': './app',
+        '@/hooks': './hooks',
+        '@/styles': './styles',
+        '@/types': './types',
+        '@/public': './public'
+      }
+    }
+    return config
+  }
+};
 
 export default nextConfig;
