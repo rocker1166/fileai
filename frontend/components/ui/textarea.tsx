3d261d05c2ff6955b7c9d2b1e7186547e8de05f7
SUMAN JANA <sumanjanaled@gmail.com>
2025-04-25 18:52:12 +0530

    fix ui  deployment fix

--- frontend/components/ui/textarea.tsx
+++ frontend/components/ui/textarea.tsx
@@ -1,5 +1,5 @@
 import * as React from "react"
-import { cn } from "../../utils"
+import { cn } from "@/lib/utils"
 
 export interface TextareaProps extends Omit<React.ComponentProps<"textarea">, "maxRows"> {
   maxRows?: number
